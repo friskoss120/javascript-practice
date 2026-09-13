1783b81059d42806b997f2abbc4995bd8d6494f9
friskoss120 <thecharasdk@proton.me>
2026-09-13 03:52:48 +0330

    feat: add basics

---
+++ basics.js
@@ -0,0 +1,16 @@
+// Variables, types, and operators
+var name = 'Alice';
+let age = 25;
+const greeting = 'Hello';
+var num1 = 10;
+var num2 = 3;
+var sum = num1 + num2;
+var product = num1 * num2;
+console.log(name);
+console.log(age);
+console.log(greeting);
+console.log('Sum:', sum);
+console.log('Product:', product);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof greeting);
